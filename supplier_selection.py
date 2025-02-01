fd48f12535d440576b691edfdc5e0b067c9f9b25
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-02-01 13:05:47 +0200

    Update supplier_selection.py

--- supplier_selection.py
+++ supplier_selection.py
@@ -52,14 +52,12 @@ def solve_supplier_selection_problem(num_weeks, w1, w2, w3, num_suppliers, suppl
       
     results = {
         "Status": LpStatus[model.status],
-        "Total Cost": model.objective.value(),
-        "Weekly Demand": [],
+        "Total Cost": round(model.objective.value(),2),
         "Details": []
     }
 
     for (s, t), allocation in selected_allocations.items():
-        results['Details'].append({"Week": t + 1, "Supplier": s + 1, "Allocation": round(allocation, 0)})
-        results["Weekly Demand"].append(weekly_demand[t])
+        results['Details'].append({"Week": t + 1, "Demand": weekly_demand[t], "Supplier": s + 1, "Allocation": round(allocation, 0)})
 
     
 
