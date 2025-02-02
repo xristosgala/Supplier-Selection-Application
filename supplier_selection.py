60e8351de6e0ab329ec02f05b4ea3da76e5493e5
xristosgala <100777920+xristosgala@users.noreply.github.com>
2025-02-02 09:44:19 +0200

    Update supplier_selection.py

--- supplier_selection.py
+++ supplier_selection.py
@@ -114,7 +114,7 @@ w3 = st.sidebar.number_input("Quality Weight", min_value=0.0, max_value=1.0, val
 num_suppliers = st.sidebar.number_input("Number of Suppliers", min_value=1, max_value=10, value=3)
 suppliers =  [i for i in range(num_suppliers)]
 costs = {supplier: st.sidebar.number_input(f"Cost for Supplier {supplier+1}", min_value=1, max_value=100, value=10) for supplier in suppliers}
-lead_times = {supplier: st.sidebar.number_input(f"Lead Time for Supplier {supplier+1}", min_value=1, max_value=100, value=12) for supplier in suppliers}
+lead_times = {supplier: st.sidebar.number_input(f"Lead Time for Supplier {supplier+1}", min_value=1, max_value=100, value=2) for supplier in suppliers}
 quality_scores = {supplier: st.sidebar.number_input(f"Quality Score for Supplier {supplier+1}", min_value=1, max_value=100, value=8) for supplier in suppliers}
 capacities = {supplier: st.sidebar.number_input(f"Capacity for Supplier {supplier+1}", min_value=1, max_value=1000, value=100) for supplier in suppliers}
 min_order = {supplier: st.sidebar.number_input(f"Minimum Order for Supplier {supplier+1}", min_value=10, max_value=100, value=10) for supplier in suppliers}
